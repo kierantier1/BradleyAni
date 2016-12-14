a2ad569a54dba6e36635667f21e574d8fe62de6c
schmk3258 <schmk3258@FHC-IC0034074.wrdsb.ca>
2016-12-14 14:23:48 -0500

    launch ayyyyyyyyyyy

---
+++ desktop/src/gdx/ani1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gdx.ani1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gdx.ani1.GdxAni1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxAni1(), config);
+	}
+}
